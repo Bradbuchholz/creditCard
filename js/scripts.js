56cfb4c46d1ff471bd7b5af0e570e81e395985d5
Shelby Trodden <shelby.trodden@gmail.com>
2022-04-19 22:04:00 -0500

    corrected numberlenght and valid functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,30 @@ function parseString(text) {
   return parsed;
   
 }
-
 console.log(parseString)
 
 function numberLength(parsed) {
   let result;
-  if (parsed >= 15) {
-    result = true;
-  } else if (parsed < 17) {
+  if (parsed.length === 15 || parsed.length === 16) {
     result = true;
   } else {
     result = false;
   }
-  return result;
-}  
+}
+
+function valid(parsed) {
+  let result;
+  if (parsed[0] === 3) { 
+    if (parsed[1] === 4 || parsed[1] === 7)
+    result = true;
+  } else if (parsed[0] >= 4 && parsed[0]<= 6) {
+    result = true;
+  } else {
+    result = false;
+    }
+  }
 
 
+
+  
+  
